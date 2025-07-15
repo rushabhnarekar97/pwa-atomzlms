388b5cbbe187fdbd986afb5d0741ad835e78492f
Rushabh Narekar <rushabh@gvmtechnologies.com>
2025-07-15 11:48:56 +0000

    <div class="relative mx-auto border-gray-800 dark:border-gray-800 bg-gra

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,12 @@ export default function RootLayout({
         <meta name="theme-color" content="#64B5F6" />
       </head>
       <body className="font-body antialiased bg-muted flex items-center justify-center min-h-screen p-4">
-        <div className="w-[375px] h-[812px] mobile-mockup">
-            <div className="mobile-mockup-screen w-full h-full">
+        <div className="relative mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[812px] w-[375px] shadow-2xl">
+            <div className="h-[32px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -start-[17px] top-[72px] rounded-s-lg"></div>
+            <div className="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -start-[17px] top-[124px] rounded-s-lg"></div>
+            <div className="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -start-[17px] top-[178px] rounded-s-lg"></div>
+            <div className="h-[64px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -end-[17px] top-[142px] rounded-e-lg"></div>
+            <div className="rounded-[2rem] overflow-hidden w-full h-full bg-background">
                 <div className="h-full w-full overflow-y-auto">
                     {children}
                     <Toaster />
